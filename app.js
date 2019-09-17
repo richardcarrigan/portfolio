0930ebb259aa2c1a9447dcf143854292023781c2
Richard Carrigan <nihonjinboy85@gmail.com>
2019-09-16 19:04:14 -0700

    Add top app bar, tab bar, and change default styling

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+import { MDCTopAppBar } from '@material/top-app-bar';
+import { MDCTabBar } from '@material/tab-bar';
+
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
