71eb187bd9eec12f29cb5c4f117439ed789868b0
Richard Carrigan <nihonjinboy85@gmail.com>
2019-09-16 23:44:04 -0700

    Add drawer and page sections, connect nav links

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCTabBar } from '@material/tab-bar';
+import { MDCDrawer } from '@material/drawer';
 
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer--modal'));
+
+topAppBar.setScrollTarget(document.getElementById('main-content'));
+topAppBar.listen('MDCTopAppBar:nav', () => {
+    drawer.open = !drawer.open;
+});
