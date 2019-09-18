2e65e8df9f564ec2f66874565280f1fe9e4e4de5
Richard Carrigan <nihonjinboy85@gmail.com>
2019-09-18 12:58:55 -0700

    Add contact form and social buttons

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCTabBar } from '@material/tab-bar';
 import { MDCDrawer } from '@material/drawer';
+import {MDCFormField} from '@material/form-field';
+import {MDCTextField} from '@material/textfield';
 
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
+const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
-const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer--modal'));
+const drawer = new MDCDrawer(document.querySelector('.mdc-drawer--modal'));
+const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
