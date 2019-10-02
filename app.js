a379ab31be09a97a666eefdbccb2ed32ac0b2194
Richard Carrigan <nihonjinboy85@gmail.com>
2019-10-02 12:25:13 -0700

    Fix onscroll event not firing issue, add About Me content

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCTabBar } from '@material/tab-bar';
 import { MDCDrawer } from '@material/drawer';
-import {MDCFormField} from '@material/form-field';
-import {MDCTextField} from '@material/textfield';
 
+// Instantiation
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 const drawer = new MDCDrawer(document.querySelector('.mdc-drawer--modal'));
-const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
-const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
 });
+
+window.onscroll = () => {
+    console.log('scrolling...');
+};
