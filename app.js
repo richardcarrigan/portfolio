8358620c628c979f8fa4713e1df65335dee5db5d
Richard Carrigan <nihonjinboy85@gmail.com>
2019-10-08 23:53:41 -0700

    Fill in content and make final style tweaks

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import { MDCTopAppBar } from '@material/top-app-bar';
 import { MDCTabBar } from '@material/tab-bar';
 import { MDCDrawer } from '@material/drawer';
+import {MDCTextField} from '@material/textfield';
 
 // Instantiation
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 const drawer = new MDCDrawer(document.querySelector('.mdc-drawer--modal'));
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 
 topAppBar.setScrollTarget(document.getElementById('main-content'));
 topAppBar.listen('MDCTopAppBar:nav', () => {
