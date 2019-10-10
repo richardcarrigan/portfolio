e115ee397b267a429d9cc66bfe26634c85ff8059
Richard Carrigan <nihonjinboy85@gmail.com>
2019-10-10 14:52:36 -0700

    Highlight corresponding nav item on scroll

--- app.js
+++ app.js
@@ -14,6 +14,28 @@ topAppBar.listen('MDCTopAppBar:nav', () => {
     drawer.open = !drawer.open;
 });
 
+let navbar = document.getElementById('navbar');
+
+let sections = document.querySelectorAll('.content-section');
+
 window.onscroll = () => {
-    console.log('scrolling...');
+    let activeTab = document.querySelector('.mdc-tab--active');
+    let activeTabIndicator = document.querySelector('.mdc-tab-indicator--active');
+
+    for(let i = sections.length - 1; i >= 0; i--) {
+
+        if(sections[i].getBoundingClientRect().top <= navbar.offsetHeight && sections[i] !== activeTab) {
+            if(activeTab) { 
+                activeTab.classList.remove('mdc-tab--active');
+                activeTabIndicator.classList.remove('mdc-tab-indicator--active');
+            };
+            if(document.getElementById(sections[i].id + '-tab') {
+                const tab = document.getElementById(sections[i].id + '-tab');
+                const tabIndicator = document.getElementById(sections[i].id + '-tab-indicator');
+                tab.classList.add('mdc-tab--active');
+                tabIndicator.classList.add('mdc-tab-indicator--active');
+            }
+            break;
+        }
+    }
 };
