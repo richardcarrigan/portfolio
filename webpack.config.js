4374d8dfa0bc055cd70c28dcc18311242be9438f
Richard Carrigan <rcarrigan@ext.aerotek.com>
2022-03-25 11:46:19 -0700

    Move all assets to assets folder and bundle appropriately

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const path = require('path');
 
 module.exports = [
   {
-    entry: ['./src/index.js'],
+    entry: ['./src/app.js', './src/app.scss'],
     mode: 'development',
+    devServer: {
+      static: './dist'
+    },
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist')
@@ -11,20 +14,22 @@ module.exports = [
     module: {
       rules: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource'
-        },
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource'
-        },
-        {
           test: /\.s[ac]ss$/i,
           use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(pdf|png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource'
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
         }
       ]
     }
