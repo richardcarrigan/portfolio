91a883193798a5b47e0b788c0cc24d554ec6235f
Richard Carrigan <me@richardcarrigan.com>
2022-10-29 23:03:47 -0700

    Fix header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,108 +2,116 @@ import { useState } from 'react';
 import {
   AppBar,
   Box,
-  Toolbar,
-  IconButton,
-  Typography,
-  Menu,
-  Container,
   Button,
-  MenuItem,
-  Link
+  // Container,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  // Menu,
+  // MenuIcon,
+  // MenuItem,
+  Toolbar,
+  Typography
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
-const pages = ['About', 'Work', 'Contact'];
+const drawerWidth = 240;
+const navItems = ['Work', 'Contact'];
 
-export default function Header() {
-  const [anchorElNav, setAnchorElNav] = useState(null);
+export default function Header(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
 
-  const handleOpenNavMenu = event => {
-    setAnchorElNav(event.currentTarget);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant='h6' sx={{ my: 2 }}>
+        Richard <span id='last-name'>Carrigan</span>
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map(item => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <Button href={`#${item.toLowerCase()}`}>
+                <ListItemText primary={item} />
+              </Button>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <AppBar position='fixed'>
-      <Container maxWidth='xl'>
-        <Toolbar disableGutters>
-          <Typography
-            variant='h6'
-            noWrap
-            component='div'
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+    <Box sx={{ display: 'flex' }}>
+      <AppBar component='nav'>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            edge='start'
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
           >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             Richard <span id='last-name'>Carrigan</span>
           </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size='large'
-              aria-label='account of current user'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
-              onClick={handleOpenNavMenu}
-              color='inherit'
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left'
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' }
-              }}
-            >
-              {pages.map(page => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Link href={`#${page.toLowerCase()}`}>
-                    <Typography
-                      textAlign='center'
-                      href={`#${page.toLowerCase()}`}
-                    >
-                      {page}
-                    </Typography>
-                  </Link>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant='h6'
-            noWrap
-            component='div'
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            Richard <span id='last-name'>Carrigan</span>
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map(page => (
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map(item => (
               <Button
-                key={page}
-                href={`#${page.toLowerCase()}`}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                href={`#${item.toLowerCase()}`}
+                key={item}
+                sx={{ color: '#fff' }}
               >
-                {page}
+                {item}
               </Button>
             ))}
           </Box>
         </Toolbar>
-      </Container>
-    </AppBar>
+      </AppBar>
+      <Box component='nav'>
+        <Drawer
+          container={container}
+          variant='temporary'
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth
+            }
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
   );
 }
+
+//                 <Button href={`#${page.toLowerCase()}`}>{page}</Button>
+//             <Button
+//               key={page}
+//               href={`#${page.toLowerCase()}`}
+//               onClick={handleCloseNavMenu}
+//               sx={{ my: 2, color: 'white', display: 'block' }}
+//             >
+//               {page}
+//             </Button>
